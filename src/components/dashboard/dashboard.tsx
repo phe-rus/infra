ad76766eacb1482ee126c2013d50eb01d335c004
la niina <la.niina.me@gmail.com>
2026-08-09 14:07:07 +0300

    feat: implement collapsible responsive sidebar and update workspace dashboard layout

--- src/components/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard.tsx
@@ -1,13 +1,38 @@
-import { type PropsWithChildren, type FC } from "react"
-import { Link } from "@tanstack/react-router"
+import { createContext, useContext, type PropsWithChildren, type FC, useState, useRef, useEffect, useCallback } from "react"
+import { useIsMobile } from "@/lib/use-media-query"
 import { Button } from "@/components/ui/button"
+import { Link } from "@tanstack/react-router"
 import { cn } from "@/lib/utils"
+import { IconChevronLeft, IconChevronRight } from "@tabler/icons-react"
 
 type DashboardProps = PropsWithChildren<{}>
+type SidebarProps = {}
+const SidebarContext = createContext<SidebarProps | null>(null)
+export function useSidebar() {
+    const context = useContext(SidebarContext)
+    if (!context) {
+        throw new Error("useDashboardContext must be used within Dashboard")
+    }
+    return context
+}
 
 export const Dashboard: FC<DashboardProps> = ({
     children
 }) => {
+    const [open, setOpen] = useState<boolean>(false)
+    const ref = useRef<HTMLDivElement>(null)
+    const isMobile = useIsMobile()
+
+    const toggleSidebar = useCallback(() => {
+        setOpen((prev) => !prev)
+    }, [])
+
+    useEffect(() => {
+        if (!isMobile) {
+            setOpen(true)
+        }
+    }, [isMobile])
+
     const navLists = [
         {
             label: "Providers",
@@ -48,44 +73,79 @@ export const Dashboard: FC<DashboardProps> = ({
     ]
 
     return (
-        <main className='fixed flex inset-0'>
-            <aside className={cn(
-                'flex flex-col w-78 leading-loose',
-                'relative h-dvh overflow-y-auto',
-                'no-scrollbar scroll-smooth'
-            )}>
-                <section className='flex flex-col gap-5 p-5'>
-                    <nav>
-                        <h1>Pherus</h1>
-                    </nav>
-                    <nav className='flex flex-col'>
-                        {navLists.map(nav => (
-                            <Link
-                                to={nav.path}
-                                className='text-xl'
-                                activeOptions={{
-                                    exact: true,
-                                    includeHash: true,
-                                    includeSearch: true
-                                }}
-                            >
-                                {nav.label}
-                            </Link>
-                        ))}
-                    </nav>
-                </section>
-                <span className="flex-1" />
-                <nav className={cn(
-                    'sticky bottom-0 p-5 mb-auto',
-                    'flex flex-col'
-                )}>
-                    <Button className='w-fit!'>Sign out</Button>
-                    <Button variant='secondary' className='w-fit!'>Create provider</Button>
-                </nav>
-            </aside>
-            <article className='flex-1'>
-                {children}
-            </article>
-        </main >
+        <SidebarContext.Provider value={{}}>
+            <main className='relative flex w-svw h-svh overflow-hidden'>
+                {open && (
+                    <div
+                        className={cn(
+                            "fixed inset-0 backdrop-blur-sm",
+                            "backdrop-blur-xs z-30 md:hidden"
+                        )}
+                        onClick={toggleSidebar}
+                    />
+                )}
+                <aside
+                    ref={ref}
+                    className={cn(
+                        "fixed md:relative shrink-0 h-full border-r backdrop-blur bg-background/85 shadow-sm",
+                        "transition-transform duration-300 ease-in-out ease-initial border-primary/5 z-55",
+                        open ? "translate-x-0 w-78" : "-translate-x-full w-fit"
+                    )}
+                >
+                    <Button
+                        size="icon-xs"
+                        variant='secondary'
+                        className={cn(
+                            "absolute top-10 -translate-y-1/2 z-56 cursor-pointer",
+                            "transition-all duration-300 select-none rounded-full",
+                            !open && 'hidden md:flex',
+                            open
+                                ? "left-full -translate-x-1/2"
+                                : "left-[calc(100%+8px)] translate-x-0.5"
+                        )}
+                        onClick={toggleSidebar}
+                    >
+                        {!open ? <IconChevronRight /> : <IconChevronLeft />}
+                    </Button>
+                    <section className={cn(
+                        "p-5 flex-col min-h-svh gap-5",
+                        !open ? "hidden" : "flex"
+                    )}>
+                        <section className='flex flex-col gap-5 p-5'>
+                            <nav>
+                                <h1>Pherus</h1>
+                            </nav>
+                            <nav className='flex flex-col'>
+                                {navLists.map((nav, index) => (
+                                    <Link
+                                        key={index}
+                                        to={nav.path}
+                                        className='text-xl'
+                                        activeOptions={{
+                                            exact: true,
+                                            includeHash: true,
+                                            includeSearch: true
+                                        }}
+                                    >
+                                        {nav.label}
+                                    </Link>
+                                ))}
+                            </nav>
+                        </section>
+                        <span className="flex-1" />
+                        <nav className={cn(
+                            'sticky bottom-0 p-5 mb-auto',
+                            'flex flex-col'
+                        )}>
+                            <Button className='w-fit!'>Sign out</Button>
+                            <Button variant='secondary' className='w-fit!'>Create provider</Button>
+                        </nav>
+                    </section>
+                </aside>
+                <article className='relative flex flex-col flex-1 overflow-y-auto no-scrollbar'>
+                    {children}
+                </article>
+            </main >
+        </SidebarContext.Provider>
     )
 }
--- src/routes/_workspace/index.tsx
+++ src/routes/_workspace/index.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router"
-import { Button } from "@/components/ui/button"
+import { IconInfoCircle } from '@tabler/icons-react'
 import { cn } from "@/lib/utils"
+import { Button, LinkButton } from "@/components/ui/button"
 
 export const Route = createFileRoute("/_workspace/")({
   component: RouteComponent
@@ -10,14 +11,65 @@ function RouteComponent() {
   return (
     <article className={cn(
       "container flex flex-col w-full md:max-w-2xl mx-auto",
-      'py-5'
+      'py-5 gap-5'
     )}>
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <h1 className="font-medium">Project ready!</h1>
-        <p>You may now add components and start building.</p>
-        <p>We&apos;ve already added the button component for you.</p>
-        <Button className="mt-2">Button</Button>
-      </div>
+      <section>
+        <h1 className='text-3xl md:text-4xl'>Good morning, Pherus</h1>
+        <p>Here&apos;s what&apos;s happening with pherus</p>
+      </section>
+
+      <section className='bg-card rounded-2xl'>
+        <div className='flex items-center gap-3 p-3'>
+          <IconInfoCircle />
+          <h2 className='text-sm flex items-center gap-1'>
+            Your code and connections all look good
+            <a className='cursor-pointer hover:underline'>View status page</a>
+          </h2>
+        </div>
+      </section>
+
+      <section className="flex flex-col gap-3">
+        <h2>Your business</h2>
+        <article className='flex items-center gap-5 p-5 rounded-2xl bg-card'>
+          <div className='flex items-center justify-evenly gap-5 w-full mx-auto'>
+            <div>
+              <h3>Monthly active users</h3>
+              <p>Last 30 days</p>
+              <h1>0</h1>
+            </div>
+            <span className="bg-border w-px h-16" />
+            <div>
+              <h3>Total users</h3>
+              <p>Current</p>
+              <h1>0</h1>
+            </div>
+          </div>
+        </article>
+      </section>
+
+      <section>
+        <div className='flex items-center justify-between'>
+          <h2>Your applications</h2>
+          <LinkButton>
+            Add application
+          </LinkButton>
+        </div>
+      </section>
+
+      <section className='bg-card rounded-2xl'>
+        <div className='flex flex-col p-10'>
+          <h1>Refer a friend and get started</h1>
+          <p>The Kinde Referral Program lets you earn rewards for customers you refer to Kinde</p>
+          <div className='mt-5 ml-auto'>
+            <Button>
+              Learn more
+            </Button>
+            <Button>
+              Share referral link
+            </Button>
+          </div>
+        </div>
+      </section>
     </article>
   )
 }
