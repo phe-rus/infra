8cd9f7999509de4ce6cd17481078fd01bd4c8d16
la niina <la.niina.me@gmail.com>
2026-08-13 22:17:20 +0300

    feat: implement OIDC and OAuth authorization server metadata endpoints with CORS support

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
+import { oauthProviderAuthServerMetadata, oauthProviderOpenIdConfigMetadata } from "@better-auth/oauth-provider";
 import handler from "@tanstack/react-start/server-entry"
+import { auth } from "./auth";
 
 export type RequestContext = {
     env: Env;
@@ -18,6 +20,32 @@ export default {
         env: Env,
         ctx: ExecutionContext,
     ) {
+        const url = new URL(request.url);
+        if (url.pathname.includes('/.well-known/openid-configuration')) {
+            const openIdHandler = oauthProviderOpenIdConfigMetadata(auth)
+            const res = await openIdHandler(request)
+            const newHeaders = new Headers(res.headers)
+            newHeaders.set("Access-Control-Allow-Methods", "GET")
+            newHeaders.set("Access-Control-Allow-Origin", "*")
+
+            return new Response(res.body, {
+                status: res.status,
+                headers: newHeaders
+            })
+        }
+        if (url.pathname.includes('/.well-known/oauth-authorization-server')) {
+            const authServerHandler = oauthProviderAuthServerMetadata(auth)
+            const res = await authServerHandler(request)
+
+            const newHeaders = new Headers(res.headers)
+            newHeaders.set("Access-Control-Allow-Methods", "GET")
+            newHeaders.set("Access-Control-Allow-Origin", "*")
+
+            return new Response(res.body, {
+                status: res.status,
+                headers: newHeaders
+            })
+        }
         return handler.fetch(request, {
             context: {
                 // @ts-ignore
