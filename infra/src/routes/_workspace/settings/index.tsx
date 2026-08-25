b620ee7bb97326cda707a4c0372f7d7feea13d38
la niina <la.niina.me@gmail.com>
2026-08-25 23:07:27 +0300

    feat: add settings workspace route structure

---
+++ infra/src/routes/_workspace/settings/index.tsx
@@ -0,0 +1,21 @@
+import { ViewController } from "@/components/views"
+import { createFileRoute } from "@tanstack/react-router"
+
+export const Route = createFileRoute("/_workspace/settings/")({
+    component: RouteComponent,
+})
+
+function RouteComponent() {
+    return (
+        <ViewController
+            heading={
+                <ViewController.Heading
+                    title="Settings"
+                    description="Manage instance settings."
+                />
+            }
+        >
+            <div>Hello "/_workspace/settings/"!</div>
+        </ViewController>
+    )
+}
